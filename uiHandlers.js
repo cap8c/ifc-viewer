192fa136f849fc8382c107f6966f7c1198b5c2bf
Cody Pennetti <53183444+cap8c@users.noreply.github.com>
2024-08-11 16:18:30 -0400

    Update uiHandlers.js

--- uiHandlers.js
+++ uiHandlers.js
@@ -4,18 +4,17 @@ export function setupUI(viewer, treeView) {
     searchBox.addEventListener("input", () => {
         const query = searchBox.value.toLowerCase();
 
-        treeView.getNodes().forEach(node => {
+        treeView.rootNode.children.forEach(node => {
             const name = node.title.toLowerCase();
             if (name.includes(query)) {
                 node.expand();
-                viewer.scene.setObjectsHighlighted([node.entity.id], true);
+                viewer.scene.setObjectsHighlighted([node.objectId], true);
             } else {
                 node.collapse();
-                viewer.scene.setObjectsHighlighted([node.entity.id], false);
+                viewer.scene.setObjectsHighlighted([node.objectId], false);
             }
         });
 
-        // If the search box is cleared, collapse all nodes and remove highlights
         if (!query) {
             treeView.collapseAllNodes();
             viewer.scene.setObjectsHighlighted([], false);
