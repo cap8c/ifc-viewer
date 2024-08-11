b6c5a8700bf3c219211797b138e866f91f8fc3ba
Cody Pennetti <53183444+cap8c@users.noreply.github.com>
2024-08-11 16:02:44 -0400

    Update uiHandlers.js

--- uiHandlers.js
+++ uiHandlers.js
@@ -1,16 +1,14 @@
-export function setupUI(viewer) {
+export function setupUI(viewer, treeView) {
     const groupByTypesButton = document.getElementById("groupByTypesButton");
     const groupByLevelsButton = document.getElementById("groupByLevelsButton");
 
     groupByTypesButton.addEventListener("click", () => {
-        const treeView = viewer.scene.plugins.TreeViewPlugin;
         treeView.groupTypes = true;
         treeView.groupLevels = false;
         treeView.refresh();  // Rebuild the tree with the new grouping
     });
 
     groupByLevelsButton.addEventListener("click", () => {
-        const treeView = viewer.scene.plugins.TreeViewPlugin;
         treeView.groupTypes = false;
         treeView.groupLevels = true;
         treeView.refresh();  // Rebuild the tree with the new grouping
