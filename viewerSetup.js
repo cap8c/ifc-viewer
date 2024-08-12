8d6e1b0e08e83035898328c41ce4e7e5350a3dc7
Cody Pennetti <53183444+cap8c@users.noreply.github.com>
2024-08-12 16:05:08 -0400

    Update viewerSetup.js

--- viewerSetup.js
+++ viewerSetup.js
@@ -1,4 +1,4 @@
-import { Viewer, WebIFCLoaderPlugin, TreeViewPlugin } from "https://cdn.jsdelivr.net/npm/@xeokit/xeokit-sdk/dist/xeokit-sdk.es.min.js";
+import { Viewer, WebIFCLoaderPlugin, TreeViewPlugin, NavCubePlugin } from "https://cdn.jsdelivr.net/npm/@xeokit/xeokit-sdk/dist/xeokit-sdk.es.min.js";
 import * as WebIFC from "https://cdn.jsdelivr.net/npm/web-ifc@0.0.51/web-ifc-api.js";
 import { setupUI } from './uiHandlers.js';
 
@@ -7,6 +7,21 @@ export function setupViewer(canvasId) {
     viewer.camera.eye = [-2.56, 8.38, 8.27];
     viewer.camera.look = [13.44, 3.31, -14.83];
     viewer.camera.up = [0.10, 0.98, -0.14];
+
+    const navCube = new NavCubePlugin(viewer, {
+
+        canvasID: "myNavCubeCanvas",
+    
+        visible: true,         // Initially visible (default)
+    
+        cameraFly: true,       // Fly camera to each selected axis/diagonal
+        cameraFitFOV: 45,      // How much field-of-view the scene takes once camera has fitted it to view
+        cameraFlyDuration: 0.5,// How long (in seconds) camera takes to fly to each new axis/diagonal
+    
+        fitVisible: false,     // Fit whole scene, including invisible objects (default)
+    
+        synchProjection: false // Keep NavCube in perspective projection, even when camera switches to ortho (default)
+    });
 
     const ifcAPI = new WebIFC.IfcAPI();
     ifcAPI.SetWasmPath("https://cdn.jsdelivr.net/npm/web-ifc@0.0.51/");
