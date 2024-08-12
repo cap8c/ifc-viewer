dc2541fefb4cdaac4866a0c3dde38c360987ddc6
Cody Pennetti <53183444+cap8c@users.noreply.github.com>
2024-08-12 13:57:58 -0400

    Update viewerSetup.js

--- viewerSetup.js
+++ viewerSetup.js
@@ -1,26 +1,13 @@
-import { Viewer, WebIFCLoaderPlugin, TreeViewPlugin, NavCubePlugin } from "https://cdn.jsdelivr.net/npm/@xeokit/xeokit-sdk/dist/xeokit-sdk.es.min.js";
+import { Viewer, WebIFCLoaderPlugin, TreeViewPlugin } from "https://cdn.jsdelivr.net/npm/@xeokit/xeokit-sdk/dist/xeokit-sdk.es.min.js";
 import * as WebIFC from "https://cdn.jsdelivr.net/npm/web-ifc@0.0.51/web-ifc-api.js";
 import { setupUI } from './uiHandlers.js';
 
 export function setupViewer(canvasId) {
-    // Initialize the viewer
     const viewer = new Viewer({ canvasId, transparent: true });
     viewer.camera.eye = [-2.56, 8.38, 8.27];
     viewer.camera.look = [13.44, 3.31, -14.83];
     viewer.camera.up = [0.10, 0.98, -0.14];
 
-    // Add the NavCube plugin
-    const navCube = new NavCubePlugin(viewer, {
-        canvasId: "myNavCubeCanvas",
-        visible: true,
-        cameraFly: true,
-        cameraFitFOV: 45,
-        cameraFlyDuration: 0.5,
-        fitVisible: false,
-        synchProjection: false
-    });
-
-   
     const ifcAPI = new WebIFC.IfcAPI();
     ifcAPI.SetWasmPath("https://cdn.jsdelivr.net/npm/web-ifc@0.0.51/");
 
