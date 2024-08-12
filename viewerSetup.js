53140057291abae7361a32d85ae9aeba30753275
Cody Pennetti <53183444+cap8c@users.noreply.github.com>
2024-08-12 10:39:19 -0400

    Update viewerSetup.js

--- viewerSetup.js
+++ viewerSetup.js
@@ -1,4 +1,4 @@
-import { Viewer, WebIFCLoaderPlugin, TreeViewPlugin } from "https://cdn.jsdelivr.net/npm/@xeokit/xeokit-sdk/dist/xeokit-sdk.es.min.js";
+import { Viewer, WebIFCLoaderPlugin, TreeViewPlugin, NavCubePlugin } from "https://cdn.jsdelivr.net/npm/@xeokit/xeokit-sdk/dist/xeokit-sdk.es.min.js";
 import * as WebIFC from "https://cdn.jsdelivr.net/npm/web-ifc@0.0.51/web-ifc-api.js";
 import { setupUI } from './uiHandlers.js';
 
