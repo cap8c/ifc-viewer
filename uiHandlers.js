3e028951861be281ca5b1fe8d4f1328289926fe4
Cody Pennetti <53183444+cap8c@users.noreply.github.com>
2024-08-12 07:19:15 -0400

    Update uiHandlers.js

--- uiHandlers.js
+++ uiHandlers.js
@@ -1,21 +1,25 @@
 export function setupUI(viewer, treeView) {
-    treeView.on("nodeTitleClicked", (e) => {
-        const scene = viewer.scene;
-        const objectIds = [];
+    if (treeView) {
+        treeView.on("nodeTitleClicked", (e) => {
+            const scene = viewer.scene;
+            const objectIds = [];
 
-        e.treeViewPlugin.withNodeTree(e.treeViewNode, (node) => {
-            if (node.objectId) {
-                objectIds.push(node.objectId);
-            }
-        });
+            treeView.withNodeTree(e.treeViewNode, (node) => {
+                if (node.objectId) {
+                    objectIds.push(node.objectId);
+                }
+            });
 
-        // X-ray all other objects
-        scene.setObjectsXRayed(scene.objectIds, true);
-        // Highlight and zoom to selected object
-        scene.setObjectsXRayed(objectIds, false);
-        viewer.cameraFlight.flyTo({
-            aabb: scene.getAABB(objectIds),
-            duration: 0.5,
+            // X-ray all other objects
+            scene.setObjectsXRayed(scene.objectIds, true);
+            // Highlight and zoom to selected object
+            scene.setObjectsXRayed(objectIds, false);
+            viewer.cameraFlight.flyTo({
+                aabb: scene.getAABB(objectIds),
+                duration: 0.5,
+            });
         });
-    });
+    } else {
+        console.error("TreeViewPlugin is not defined.");
+    }
 }
