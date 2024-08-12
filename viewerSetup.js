52aff09a720de09ca0d671664729b2d40f2e2696
Cody Pennetti <53183444+cap8c@users.noreply.github.com>
2024-08-12 16:58:15 -0400

    Update viewerSetup.js

--- viewerSetup.js
+++ viewerSetup.js
@@ -1,20 +1,28 @@
 import { Viewer, WebIFCLoaderPlugin, TreeViewPlugin, NavCubePlugin } from "https://cdn.jsdelivr.net/npm/@xeokit/xeokit-sdk/dist/xeokit-sdk.es.min.js";
 import * as WebIFC from "https://cdn.jsdelivr.net/npm/web-ifc@0.0.51/web-ifc-api.js";
+import { setupUI } from './uiHandlers.js';
 
 export function setupViewer(canvasId) {
-    // Initialize the viewer
-    const viewer = new Viewer({
-        canvasId,
-        transparent: true, // Ensure transparency
-        backgroundColor: [0, 0, 0, 0] // Fully transparent background
-    });
-
-    // Set initial camera position
+    const viewer = new Viewer({ canvasId, transparent: true,  backgroundColor: [0, 0, 0, 0] // Fully transparent background });
     viewer.camera.eye = [-2.56, 8.38, 8.27];
     viewer.camera.look = [13.44, 3.31, -14.83];
     viewer.camera.up = [0.10, 0.98, -0.14];
 
-    // Initialize WebIFC and load the model
+    const navCube = new NavCubePlugin(viewer, {
+
+        canvasID: "myNavCubeCanvas",
+    
+        visible: true,         // Initially visible (default)
+    
+        cameraFly: true,       // Fly camera to each selected axis/diagonal
+        cameraFitFOV: 45,      // How much field-of-view the scene takes once camera has fitted it to view
+        cameraFlyDuration: 0.5,// How long (in seconds) camera takes to fly to each new axis/diagonal
+    
+        fitVisible: false,     // Fit whole scene, including invisible objects (default)
+    
+        synchProjection: false // Keep NavCube in perspective projection, even when camera switches to ortho (default)
+    });
+
     const ifcAPI = new WebIFC.IfcAPI();
     ifcAPI.SetWasmPath("https://cdn.jsdelivr.net/npm/web-ifc@0.0.51/");
 
@@ -27,19 +35,15 @@ export function setupViewer(canvasId) {
         });
 
         model.on("loaded", () => {
-            console.log("Model loaded successfully.");
             viewer.cameraFlight.flyTo({ aabb: model.aabb });
 
-            // Initialize TreeViewPlugin
             const treeView = new TreeViewPlugin(viewer, {
                 containerElement: document.getElementById("treeViewContainer"),
                 autoExpandDepth: 1,
-                groupTypes: true
+                hierarchy: "types"
             });
 
-            // Add the model to the tree view
-            treeView.addModel(model);
-
+            setupUI(viewer, treeView);  // Pass treeView to setupUI
         });
 
         model.on("error", (error) => {
